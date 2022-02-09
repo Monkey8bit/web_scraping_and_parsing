ae6ca2c35cd30420b010cd87fe311ced8becb999
Monkey8bit <de_construct@mail.ru>
2022-02-09 18:22:38 +0400

    lesson_1_black_fix

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -7,7 +7,9 @@ CITY = input("Enter the city where you want to check weather: ")
 
 
 def get_coord(city):
-    res = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={WEATHER_SECRET}")
+    res = requests.get(
+        f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={WEATHER_SECRET}"
+    )
     if not res.json():
         print(f"City {city} does not exist.")
         return None
@@ -20,12 +22,16 @@ def get_weather(city):
     if not coordinates:
         return None
     lat, lon = coordinates[0], coordinates[1]
-    res = requests.get(f"http://api.openweathermap.org/data/2.5/weather?"
-                       f"units=metric&lat={lat}&lon={lon}&appid={WEATHER_SECRET}")
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?"
+        f"units=metric&lat={lat}&lon={lon}&appid={WEATHER_SECRET}"
+    )
     temp = res.json()["main"]["temp"]
     weather_type = res.json()["weather"][0]["main"]
     weather_desc = res.json()["weather"][0]["description"]
-    print(f"Today in {city} is {temp}\u2103\n{weather_type}, more precisely - {weather_desc}")
+    print(
+        f"Today in {city} is {temp}\u2103\n{weather_type}, more precisely - {weather_desc}"
+    )
 
 
 def main():
